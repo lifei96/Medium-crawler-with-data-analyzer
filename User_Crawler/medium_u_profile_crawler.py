8c9ea41018fdcb9aa467291499030af0885f2e27
Fei <lifei9696@gmail.com>
2016-01-26 23:36:05 +0800

    first commit

---
+++ User_Crawler/medium_u_profile_crawler.py
@@ -0,0 +1,50 @@
+# encoding: utf-8
+import random
+import time
+from selenium import webdriver
+
+class User(object):
+	def __init__(self):
+		super(User, self).__init__()
+		self.data = {
+			'ID' : -1,#帐号
+			'Name' : -1,#姓名
+			'Description' : "",#个人描述
+			'Following' : 0,#关注数量
+			'Followers' : 0,#被关注数量
+			'Twitter_ID' : -1,#Twitter帐号
+			'Facebook_ID' : -1,#Facebook帐号
+		}
+	
+	def getstr(self):
+		result = "{\n"\
+		+ '    \"ID\": \"' + str(self.data['ID']) + "\",\n"\
+		+ '    \"Name\": \"' + str(self.data['Name']) + "\",\n"\
+		+ '    \"Description\": \"' + str(self.data['Description']) + "\",\n"\
+		+ '    \"Following\": ' + str(self.data['Following']) + ",\n"\
+		+ '    \"Followers\": ' + str(self.data['Followers']) + ",\n"\
+		+ '    \"Twitter_ID\": \"' + str(self.data['Twitter_ID']) + "\",\n"\
+		+ '    \"Facebook_ID\": \"' + str(self.data['Facebook_ID']) + "\",\n}"
+		return result
+
+def get_profile(ID, driver):
+	url = "https://medium.com/@" + str(ID)
+	driver.get(url)
+	time.sleep(2)
+	user = User()
+	user.data['ID'] = ID
+	user.data['Name'] = driver.find_element_by_class_name("hero-title").text
+	if driver.find_element_by_class_name("hero-description ") != -1:
+		user.data['Description'] = driver.find_element_by_class_name("hero-description ").text
+	button_list = driver.find_elements_by_class_name("button")
+	for button in button_list:
+		if button.get_attribute("data-action-value") == "following":
+			user.data['Following'] = button.text.split()[0]
+		if button.get_attribute("data-action-value") == "followers":
+			user.data['Followers'] = button.text.split()[0]
+		if button.get_attribute("title") == "Twitter":
+			user.data['Twitter_ID'] = str(button.get_attribute("href"))[20:]
+		if button.get_attribute("title") == "Facebook":
+			user.data['Facebook_ID'] = str(button.get_attribute("href"))[21:]
+	return user
+
---
+++ User_Crawler/medium_u_script.py
@@ -0,0 +1,45 @@
+# encoding: utf-8
+import json
+import codecs
+import os
+import medium_u_crawler_main
+
+begin_ID = "chenyang03"
+
+class Queue:
+    def __init__(self):
+        self.items = []
+    def isEmpty(self):
+        return self.items == []
+    def enqueue(self, item):
+        self.items.insert(0,item)
+    def dequeue(self):
+        return self.items.pop()
+    def size(self):
+        return len(self.items)
+        
+def BFS(start):
+	visited = []
+	queue = Queue()
+	queue.enqueue(start)
+	while queue.size():
+		print(queue.size())
+		current = queue.dequeue()
+		visited.append(current)
+		medium_u_crawler_main.get(current)
+		input_text = codecs.open("./Data/%s_following.txt"%str(current), 'r', 'utf-8')
+		following = (str(input_text.read()).replace('\n', '').replace('"', '').replace(' ', '').replace("{following_ID:[", "").replace("]}", "")).split(',')
+		for fol in following:
+			if fol not in visited:
+				queue.enqueue(fol)
+		input_text.close()
+		input_text = codecs.open("./Data/%s_followers.txt"%str(current), 'r', 'utf-8')
+		followers = (str(input_text.read()).replace('\n', '').replace('"', '').replace(' ', '').replace("{followers_ID:[", "").replace("]}", "")).split(',')
+		for fol in followers:
+			if fol not in visited:
+				queue.enqueue(fol)
+		input_text.close()
+		print(queue.size())
+
+BFS(begin_ID)
+
