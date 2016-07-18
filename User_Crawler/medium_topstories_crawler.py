69fe7f1807c5aaae8951810dd6793d65f4f3d6bd
lifei96 <lifei9696@gmail.com>
2016-07-18 09:08:55 +0800

    Redesigned

--- User_Crawler/medium_topstories_crawler.py
+++ User_Crawler/medium_topstories_crawler.py
@@ -15,7 +15,6 @@ class TopStories(object):
         self.data = {
             'date': "",
             'url': "",
-            'num': 0,
             'stories': [],
         }
 
@@ -35,6 +34,7 @@ class Story(object):
             'tags': [],
             'recommends': 0,
             'responses': 0,
+            'success': 1,
         }
 
 
@@ -43,29 +43,33 @@ def get_story(url):
     cj = cookielib.MozillaCookieJar()
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
     req = urllib2.Request(url)
-    req.add_header("User-agent",'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) \
+    req.add_header("User-agent", 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) \
                     Chrome/50.0.2661.102 Safari/537.36')
     try:
         response = opener.open(req, timeout=10)
     except urllib2.URLError:
+        story.data['success'] = 0
         print('timeout')
         return story
     data = response.read()
 
     story_id = re.findall('data-post-id="(.*?)" data-is-icon', data)
     if not story_id:
+        story.data['success'] = 0
         print('-----fail to get story_id')
     else:
         story.data['story_id'] = story_id[0]
 
     author = re.findall('"username":"(.*?)","createdAt"', data)
     if not author:
+        story.data['success'] = 0
         print('-----fail to get author')
     else:
         story.data['author'] = author[0]
 
     timestamp = re.findall('"firstPublishedAt":(.*?),"latestPublishedAt"', data)
     if not timestamp:
+        story.data['success'] = 0
         print('-----fail to get timestamp')
     else:
         story.data['timestamp'] = float(timestamp[0])
@@ -73,18 +77,21 @@ def get_story(url):
 
     tags = re.findall('false,"tags":(.*?),"socialRecommendsCount"', data)
     if not tags:
+        story.data['success'] = 0
         print('-----fail to get tags')
     else:
         story.data['tags'] = json.loads(tags[0])
 
     recommends = re.findall('"recommends":(.*?),"socialRecommends"', data)
     if not recommends:
+        story.data['success'] = 0
         print('-----fail to get recommends')
     else:
         story.data['recommends'] = eval(recommends[0])
 
     responses = re.findall('"responsesCreatedCount":(.*?),"links"', data)
     if not responses:
+        story.data['success'] = 0
         print('-----fail to get responses')
     else:
         story.data['responses'] = eval(responses[0])
@@ -102,7 +109,7 @@ def get_top_stories():
         top_stories = TopStories()
         date_string = current_date.strftime("%B-%d-%Y").lower()
         url = "https://medium.com/browse/top/" + date_string
-        top_stories.data['date'] = date_string
+        top_stories.data['date'] = current_date.isoformat()
         top_stories.data['url'] = url
 
         cj = cookielib.MozillaCookieJar()
@@ -117,9 +124,10 @@ def get_top_stories():
         story_url = re.findall('<a class="link link--darken" href="(.*?)\?source=top_stories---------[0-9]*-" data-action="open-post"', data)
         num = len(story_url)
         for i in range(num):
-            stories.append(get_story(story_url[i]).data)
+            story_data = get_story(story_url[i]).data
+            if story_data['success']:
+                stories.append(story_data)
             print(i)
-        top_stories.data['num'] = num
         top_stories.data['stories'] = stories
 
         out = codecs.open("./TopStories/%s.txt" % current_date.isoformat(), 'w', 'utf-8')
